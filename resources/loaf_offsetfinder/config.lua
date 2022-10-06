41d9eea2db2ab5049818277bc6dafb53c514de52
luap710 <92261388+itsluap@users.noreply.github.com>
2022-10-06 01:45:19 -0500

    .

---
+++ resources/loaf_offsetfinder/config.lua
@@ -0,0 +1,11 @@
+Shells = {
+    ["standard"] = {
+        obj = `gabz_pinkcage`
+    },
+    ["classic"] = {
+        obj = `classicmotel_shell`
+    },
+    ["highend"] = {
+        obj = `highendmotel_shell`
+    },
+}
