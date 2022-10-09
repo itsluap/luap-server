3b88d31510f5872d041a9b331dc7804b4f1c4e44
itsluap <92261388+itsluap@users.noreply.github.com>
2022-10-09 18:53:57 -0500

    cayo

---
+++ resources/[qb]/[ui]/qb-doorlock/configs/cayo.lua
@@ -0,0 +1,29 @@
+
+
+-- 1 created by luap
+Config.DoorList['cayo-1'] = {
+    doorType = 'double',
+    authorizedGangs = { ['cartel'] = 0 },
+    doorLabel = 'Front Gate 2',
+    locked = true,
+    doorRate = 1.0,
+    doors = {
+        {objName = 1215477734, objYaw = 48.199947357178, objCoords = vec3(4987.587402, -5718.634766, 20.781033)},
+        {objName = -1574151574, objYaw = 49.30001449585, objCoords = vec3(4990.681152, -5715.105957, 20.781033)}
+    },
+    distance = 2,
+}
+
+-- 2 created by luap
+Config.DoorList['cayo-2'] = {
+    doorType = 'double',
+    authorizedGangs = { ['cartel'] = 0 },
+    doorLabel = 'Front Gate 1',
+    locked = true,
+    doorRate = 1.0,
+    doors = {
+        {objName = -1574151574, objYaw = 228.19996643066, objCoords = vec3(4981.012207, -5712.747070, 20.781033)},
+        {objName = 1215477734, objYaw = 228.19996643066, objCoords = vec3(4984.133789, -5709.248535, 20.781033)}
+    },
+    distance = 2,
+}
