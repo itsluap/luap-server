2ecb6b86a05ce3248643936540e8ec1e0f5b7cd0
luap710 <92261388+itsluap@users.noreply.github.com>
2022-10-16 11:38:13 -0500

    Update seatbelt.lua

--- resources/[qb]/[base]/qb-smallresources/client/seatbelt.lua
+++ resources/[qb]/[base]/qb-smallresources/client/seatbelt.lua
@@ -28,9 +28,10 @@ local function EjectFromVehicle()
     SetPedToRagdoll(ped, 5511, 5511, 0, 0, 0, 0)
     SetEntityVelocity(ped, veloc.x*4,veloc.y*4,veloc.z*4)
     local ejectspeed = math.ceil(GetEntitySpeed(ped) * 8)
-    --[[
     if GetEntityHealth(ped) - ejectspeed > 0 then
-        SetEntityHealth(ped, GetEntityHealth(ped) - ejectspeed) -- changes player health on eject
+        SetEntityHealth(ped, GetEntityHealth(ped) - 20) -- changes player health on eject
+    end
+    --[[
     elseif GetEntityHealth(ped) ~= 0 then
         SetEntityHealth(ped, 0) -- kills player on eject
     end
