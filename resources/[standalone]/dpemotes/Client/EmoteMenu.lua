381db09ac1ec069baeddc0ec545f26ec5556a519
luap710 <92261388+itsluap@users.noreply.github.com>
2022-10-16 12:11:47 -0500

    Update EmoteMenu.lua

--- resources/[standalone]/dpemotes/Client/EmoteMenu.lua
+++ resources/[standalone]/dpemotes/Client/EmoteMenu.lua
@@ -294,6 +294,22 @@ end
 
 function OpenEmoteMenu()
     mainMenu:Visible(not mainMenu:Visible())
+    local veh = GetVehiclePedIsIn(PlayerPedId(), false)
+    if DoesEntityExist(veh) and not IsEntityDead(veh) then
+        local model = GetEntityModel(veh)
+        -- If it's not a boat, plane or helicopter, and the vehilce is off the ground with ALL wheels, then block steering/leaning left/right/up/down.
+        if not IsThisModelABoat(model) and not IsThisModelAHeli(model) and not IsThisModelAPlane(model) and not IsThisModelABike(model) and not IsThisModelABicycle(model) and IsEntityInAir(veh) then
+            print("in the air")
+            DisableControlAction(0, 59) -- leaning left/right
+            DisableControlAction(0, 60) -- leaning up/down
+        end
+        local roll = GetEntityRoll(vehicle)
+        if (roll > 75.0 or roll < -75.0) and GetEntitySpeed(vehicle) < 2 then
+            print("disabling roll")
+            DisableControlAction(2,59,true) -- Disable left/right
+            DisableControlAction(2,60,true) -- Disable up/down
+        end
+    end
 end
 
 function firstToUpper(str)
