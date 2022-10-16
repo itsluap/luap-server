e995cf939b13cf39d677b8e3401ac7666a3140b8
luap710 <92261388+itsluap@users.noreply.github.com>
2022-10-16 12:34:46 -0500

    Update EmoteMenu.lua

--- resources/[standalone]/dpemotes/Client/EmoteMenu.lua
+++ resources/[standalone]/dpemotes/Client/EmoteMenu.lua
@@ -314,24 +314,6 @@ _menuPool:RefreshIndex()
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-        if _menuPool:IsAnyMenuOpen() then
-          local veh = GetVehiclePedIsIn(PlayerPedId(), false)
-          if DoesEntityExist(veh) and not IsEntityDead(veh) then
-              local model = GetEntityModel(veh)
-              -- If it's not a boat, plane or helicopter, and the vehilce is off the ground with ALL wheels, then block steering/leaning left/right/up/down.
-              if not IsThisModelABoat(model) and not IsThisModelAHeli(model) and not IsThisModelAPlane(model) and not IsThisModelABike(model) and not IsThisModelABicycle(model) and IsEntityInAir(veh) then
-                  print("in the air")
-                  DisableControlAction(0, 59) -- leaning left/right
-                  DisableControlAction(0, 60) -- leaning up/down
-              end
-              local roll = GetEntityRoll(vehicle)
-              if (roll > 75.0 or roll < -75.0) and GetEntitySpeed(vehicle) < 2 then
-                  print("disabling roll")
-                  DisableControlAction(2,59,true) -- Disable left/right
-                  DisableControlAction(2,60,true) -- Disable up/down
-              end
-          end
-        end
         _menuPool:ProcessMenus()
     end
 end)
