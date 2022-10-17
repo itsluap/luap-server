43386c7eae3521ff408dcb4f5dda2ff4812d71ac
luap710 <92261388+itsluap@users.noreply.github.com>
2022-10-17 16:12:06 -0500

    Update nobhop.lua

--- resources/[qb]/[base]/qb-assets/client/nobhop.lua
+++ resources/[qb]/[base]/qb-assets/client/nobhop.lua
@@ -10,6 +10,7 @@ Citizen.CreateThread(function()
       if IsControlPressed(0, 22) and IsPedOnFoot(ped) and not IsPedSwimming(ped) and (IsPedRunning(ped) or IsPedSprinting(ped)) and not IsPedClimbing(ped) and IsPedJumping(ped) and not IsPedRagdoll(ped) then
         print("jump")
         Jump = Jump + 1
+        print(Jump)
 
           if Jump == NumberJump then
 
