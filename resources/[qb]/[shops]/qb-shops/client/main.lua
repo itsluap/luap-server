52b4873d4eb7eb04d1a75232750ed0b39059d63e
luap710 <92261388+itsluap@users.noreply.github.com>
2022-10-16 22:44:40 -0500

    Update main.lua

--- resources/[qb]/[shops]/qb-shops/client/main.lua
+++ resources/[qb]/[shops]/qb-shops/client/main.lua
@@ -153,7 +153,7 @@ local function createPeds()
                         gang = v.requiredGang
                     }
                 },
-                distance = 3.5
+                distance = 2.5
             })
         end
     end
