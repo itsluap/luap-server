6073095f97055a5fc1c41c8eb68c555ef218c10d
luap710 <92261388+itsluap@users.noreply.github.com>
2022-10-19 17:36:05 -0500

    Update fleeca.lua

--- resources/[qb]/[crime]/qb-bankrobbery/client/fleeca.lua
+++ resources/[qb]/[crime]/qb-bankrobbery/client/fleeca.lua
@@ -269,7 +269,11 @@ RegisterNetEvent('electronickit:UseElectronickit', function()
                             TriggerServerEvent('qb-bankrobbery:server:removeElectronicKit')
                             --TriggerEvent("mhacking:show")
                             --TriggerEvent("mhacking:start", math.random(6, 7), math.random(12, 15), OnHackDone)
-                            exports['minigame-shape']:StartShapeGame(function(OnHackDone) end)
+                            exports['minigame-shape']:StartShapeGame(function(Outcome)
+                                if Outcome then
+                                    success = true
+                                    OnHackDone(success)
+                                end
                             if copsCalled or not Config.SmallBanks[closestBank]["alarm"] then return end
                             TriggerServerEvent("qb-bankrobbery:server:callCops", "small", closestBank, pos)
                             copsCalled = true
