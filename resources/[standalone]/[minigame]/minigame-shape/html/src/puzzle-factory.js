d46b0ef054eda17dfa32b5100e88db28004aca9b
luap710 <92261388+itsluap@users.noreply.github.com>
2022-10-21 01:12:06 -0500

    Update puzzle-factory.js

--- resources/[standalone]/[minigame]/minigame-shape/html/src/puzzle-factory.js
+++ resources/[standalone]/[minigame]/minigame-shape/html/src/puzzle-factory.js
@@ -74,6 +74,5 @@ export function generateQuestionAndAnswer(nums, puzzles){
     // this is confusing as hell, but works somehow
     const question =  `${firstQuestion} (${nums[positionOne]}) AND ${secondQuestion} (${nums[positionTwo]})`
     const answer = QUESTIONS[firstQuestion](puzzles[positionOne]) + ' ' + QUESTIONS[secondQuestion](puzzles[positionTwo])
-    console.log('Jah dat denk ik niet')
     return [question, answer]
 }
