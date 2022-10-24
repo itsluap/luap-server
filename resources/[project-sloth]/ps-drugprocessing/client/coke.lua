35ec12638ad5d66772403341266d395f33723bab
luap710 <92261388+itsluap@users.noreply.github.com>
2022-10-24 11:53:10 -0500

    Update coke.lua

--- resources/[project-sloth]/ps-drugprocessing/client/coke.lua
+++ resources/[project-sloth]/ps-drugprocessing/client/coke.lua
@@ -375,19 +375,20 @@ end)
 CreateThread(function()
 	local coords = GetEntityCoords(PlayerPedId(source))
 	local nearcoke = false
+	while true do
+		if #(coords-Config.CircleZones.CokePowder.coords) < 5 then
+			nearcoke = true
+		else
+			nearcoke = false
+		end
 
-	if #(coords-Config.CircleZones.CokePowder.coords) < 5 then
-		nearcoke = true
-	else
-		nearcoke = false
+		if nearcoke then
+			exports['ps-ui']:StatusShow("Cocaine Cutting", {
+				"Required Items: 1x Fine Scale, 5x Baking Soda, 10x Cocaine",
+			})
+		else
+			exports['ps-ui']:StatusHide()
+		end
+		if nearcoke == false then exports['ps-ui']:StatusHide() end
 	end
-
-	if nearcoke then
-		exports['ps-ui']:StatusShow("Cocaine Cutting", {
-			"Required Items: 1x Fine Scale, 5x Baking Soda, 10x Cocaine",
-		})
-	else
-		exports['ps-ui']:StatusHide()
-	end
-	if nearcoke == false then exports['ps-ui']:StatusHide() end
 end)
