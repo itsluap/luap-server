0c38787a76c60dc87ba64b28d4f26e50d62b6434
luap710 <92261388+itsluap@users.noreply.github.com>
2022-10-24 21:50:48 -0500

    Update consumables.lua

--- resources/[qb]/[base]/qb-smallresources/client/consumables.lua
+++ resources/[qb]/[base]/qb-smallresources/client/consumables.lua
@@ -246,7 +246,7 @@ end)
 
 -- testing new eat event --
 RegisterNetEvent('qb-items:client:eat')
-AddEventHandler('qb-items:client:eat', function(ItemName, PropName)
+AddEventHandler('qb-items:client:eat', function(itemName, PropName)
     exports['qb-assets']:AddProp(PropName)
     --TriggerEvent('lj-inventory:client:set:busy', true)
     exports['qb-assets']:RequestAnimationDict("mp_player_inteat@burger")
