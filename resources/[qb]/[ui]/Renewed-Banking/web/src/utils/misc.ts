fa317349b0516361f71139b255c3173892736fc8
luap710 <92261388+itsluap@users.noreply.github.com>
2022-10-25 02:00:53 -0500

    .

--- resources/[qb]/[ui]/Renewed-Banking/web/src/utils/misc.ts
+++
@@ -1,5 +0,0 @@
-export const isEnvBrowser = (): boolean => !(window as any).invokeNative;
-
-export function formatMoney(number: number) {
-    return number.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
-}
