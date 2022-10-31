638718df3aa8140a88c9b6eb10845a8aaa5cd756
luap710 <92261388+itsluap@users.noreply.github.com>
2022-10-31 15:59:45 -0500

    Update __resource.lua

--- resources/[assets]/[maps]/city-maps/__resource.lua
+++ resources/[assets]/[maps]/city-maps/__resource.lua
@@ -3,14 +3,15 @@ resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 this_is_a_map 'yes'
 
 data_file 'TIMECYCLEMOD_FILE' 'nutt_timecycle_mods_1.xml'
+
 files {
          "nutt_timecycle_mods_1.xml",
 }
 
 files {
-    'shellprops.ytyp'
+    'shellprops.ytyp',
     'stream/bossroom/gta5.meta',
-    'stream/bossroom/water.xml'
+    'stream/bossroom/water.xml',
 }
 
 data_file 'DLC_ITYP_REQUEST' 'shellprops.ytyp'
