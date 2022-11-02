1ff9bd77c723bca5a52a1d9dbfe314f2decb1cdf
luap710 <92261388+itsluap@users.noreply.github.com>
2022-11-02 10:48:34 -0500

    Update config.lua

--- resources/[qb]/[ui]/qb-scenes/shared/config.lua
+++ resources/[qb]/[ui]/qb-scenes/shared/config.lua
@@ -1,5 +1,5 @@
 Config = {}
 
-Config.AdminOnly = false -- Only Admins will be able to use this NUI
+Config.AdminOnly = true -- Only Admins will be able to use this NUI
 Config.MaxPlacementDistance = 25.0
 Config.AuditInterval = 1000 * 60 * 15 -- Timeframe in milisections on how often the database will be queried to check for expired drawtexts
