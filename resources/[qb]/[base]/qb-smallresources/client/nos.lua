5f532a3bbd78c295739e4682107ffdea4aac49ee
luap710 <92261388+itsluap@users.noreply.github.com>
2022-11-05 02:44:34 -0500

    Update nos.lua

--- resources/[qb]/[base]/qb-smallresources/client/nos.lua
+++ resources/[qb]/[base]/qb-smallresources/client/nos.lua
@@ -252,6 +252,11 @@ RegisterNetEvent('nitrous:client:UnloadNitrous', function(Plate)
     end
 end)
 
+function CreateVehiclePurgeSpray(vehicle, xOffset, yOffset, zOffset, xRot, yRot, zRot, scale)
+    UseParticleFxAssetNextCall('core')
+    return StartParticleFxLoopedOnEntity('ent_sht_steam', vehicle, xOffset, yOffset, zOffset, xRot, yRot, zRot, scale, false, false, false)
+end
+
 function SetVehicleNitroPurgeEnabled(vehicle, enabled)
     if enabled then
       local bone = GetEntityBoneIndexByName(vehicle, 'bonnet')
