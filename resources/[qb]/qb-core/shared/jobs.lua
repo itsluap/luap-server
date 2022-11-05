f4b991795abbd4ec881c478490b1b144280cc653
luap710 <92261388+itsluap@users.noreply.github.com>
2022-11-04 22:40:34 -0500

    Update jobs.lua

--- resources/[qb]/qb-core/shared/jobs.lua
+++ resources/[qb]/qb-core/shared/jobs.lua
@@ -207,6 +207,44 @@ QBShared.Jobs = {
             },
         },
 	},
+    ['ottos'] = {
+		label = 'Ottos Autos',
+        type = "ottos",
+		defaultDuty = true,
+		offDutyPay = false,
+		grades = {
+            ['0'] = {
+                name = 'Recruit',
+                payment = 50
+            },
+			['1'] = {
+                name = 'Novice',
+                payment = 75
+            },
+			['2'] = {
+                name = 'Experienced',
+                payment = 100
+            },
+			['3'] = {
+                name = 'Advanced',
+                payment = 125
+            },
+			['4'] = {
+                name = 'Manager',
+				isboss = true,
+                --bankAuth = true,
+                payment = 150
+            },
+            ['5'] = {
+                name = 'Owner',
+                isboss = true,
+                bankAuth = true,
+				isboss = true,
+                bankAuth = true,
+                payment = 350
+            },
+        },
+	},
     ["burgershot"] = {
 		label = "Burgershot Employee",
 		defaultDuty = true,
