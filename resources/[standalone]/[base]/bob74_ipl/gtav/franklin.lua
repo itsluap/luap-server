af1ef98f50ae6018a4a4bbd211d17c42876a7fc8
luap710 <92261388+itsluap@users.noreply.github.com>
2022-11-06 14:14:27 -0600

    Update franklin.lua

--- resources/[standalone]/[base]/bob74_ipl/gtav/franklin.lua
+++ resources/[standalone]/[base]/bob74_ipl/gtav/franklin.lua
@@ -61,7 +61,7 @@ Franklin = {
     },
 
     LoadDefault = function()
-        Franklin.Style.Set(Franklin.Style.settled)
+        Franklin.Style.Set(Franklin.Style.empty)
         Franklin.GlassDoor.Set(Franklin.GlassDoor.opened)
         Franklin.Details.Enable(Franklin.Details.flyer, false)
         Franklin.Details.Enable(Franklin.Details.tux, false)
