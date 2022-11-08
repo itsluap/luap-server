57a6ed5bb54647bf9df90274e8fa7f8161e0acb9
itsluap <92261388+itsluap@users.noreply.github.com>
2022-11-08 14:59:15 -0600

    .

--- resources/[qb]/[phone]/qb-phone/server/employment.lua
+++ resources/[qb]/[phone]/qb-phone/server/employment.lua
@@ -22,7 +22,7 @@ local function getMyJobs(cid)
     return jobs, employees
 end
 
-local FirstStart = false
+local FirstStart = true
 
 CreateThread(function()
     ---- Convertion Tool I guess LOL ----
