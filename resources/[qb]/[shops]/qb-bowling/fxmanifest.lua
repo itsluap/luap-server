c4434c081d588d16ec531daa88901eda9d3e7881
luap710 <92261388+itsluap@users.noreply.github.com>
2022-11-08 21:33:50 -0600

    add qb-bowling

---
+++ resources/[qb]/[shops]/qb-bowling/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+client_scripts {
+  'client/cl_*.lua',
+}
+
+shared_script {
+  'sh_config.lua',
+}
+
+server_scripts {
+  'server/sv_*.lua',
+}
+
+ui_page ('ui/index.html')
+
+files {
+  'ui/*'
+}
+
