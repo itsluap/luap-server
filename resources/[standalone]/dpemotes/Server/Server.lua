bc76e949875c7b45f3795b3f869fbe8ad5988a2f
luap710 <92261388+itsluap@users.noreply.github.com>
2022-11-08 19:05:43 -0600

    remove server print from emotes

--- resources/[standalone]/dpemotes/Server/Server.lua
+++ resources/[standalone]/dpemotes/Server/Server.lua
@@ -86,8 +86,6 @@ if Config.SqlKeybinding then
 			end)
 		end
 	end)
-else
-	print("[dp] ^3Sql Keybinding^7 is turned ^1off^7, if you want to enable /emotebind, import dpkeybinding.sql and set ^3SqlKeybinding = ^2true^7 in config.lua.")
 end
 
 QBCore.Functions.CreateUseableItem("walkstick", function(source, item)
