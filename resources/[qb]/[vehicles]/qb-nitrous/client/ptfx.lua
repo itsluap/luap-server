9ed9c56b93fcae80a10135bbfac85cbe73b10460
itsluap <92261388+itsluap@users.noreply.github.com>
2022-11-09 19:36:06 -0600

    remove qb-nitrous

--- resources/[qb]/[vehicles]/qb-nitrous/client/ptfx.lua
+++
@@ -1,5 +0,0 @@
-
-function CreateVehiclePurgeSpray(vehicle, xOffset, yOffset, zOffset, xRot, yRot, zRot, scale)
-    UseParticleFxAssetNextCall('core')
-    return StartParticleFxLoopedOnEntity('ent_sht_steam', vehicle, xOffset, yOffset, zOffset, xRot, yRot, zRot, scale, false, false, false)
-end
