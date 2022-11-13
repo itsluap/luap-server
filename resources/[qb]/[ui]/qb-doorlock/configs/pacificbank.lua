8610671853ad2cce917cf37dad94b137122dfe7a
itsluap <92261388+itsluap@users.noreply.github.com>
2022-11-13 14:55:01 -0600

    Create pacificbank.lua

---
+++ resources/[qb]/[ui]/qb-doorlock/configs/pacificbank.lua
@@ -0,0 +1,119 @@
+
+
+-- 1 created by luap
+Config.DoorList['pacificbank-1'] = {
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    objCoords = vec3(267.369965, 213.407990, 106.370247),
+    objName = -2121568016,
+    doorLabel = 'clerk door',
+    doorRate = 1.0,
+    pickable = true,
+    fixText = false,
+    distance = 2,
+    objYaw = 250.00028991699,
+    locked = true,
+}
+
+-- 2 created by luap
+Config.DoorList['pacificbank-2'] = {
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    objCoords = vec3(270.230652, 221.267303, 106.370247),
+    objName = -2121568016,
+    doorLabel = 'clerk door 2',
+    doorRate = 1.0,
+    pickable = true,
+    fixText = false,
+    distance = 2,
+    objYaw = 70.000221252441,
+    locked = true,
+}
+
+-- 3 created by luap
+Config.DoorList['pacificbank-3'] = {
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    objCoords = vec3(256.606812, 229.689621, 106.370247),
+    objName = -2121568016,
+    doorLabel = 'clerk door 3',
+    doorRate = 1.0,
+    pickable = true,
+    fixText = false,
+    distance = 2,
+    objYaw = 70.000221252441,
+    locked = true,
+}
+
+-- 4 created by luap
+Config.DoorList['pacificbank-4'] = {
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    objCoords = vec3(251.519867, 215.713150, 106.370247),
+    objName = -2121568016,
+    doorLabel = 'clerk door 4',
+    doorRate = 1.0,
+    pickable = true,
+    fixText = false,
+    distance = 2,
+    objYaw = 250.00028991699,
+    locked = true,
+}
+
+-- 5 created by luap
+Config.DoorList['pacificbank-5'] = {
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    objCoords = vec3(277.595337, 223.541580, 106.380432),
+    objName = 267980221,
+    doorLabel = 'stairs 1',
+    doorRate = 1.0,
+    pickable = true,
+    fixText = false,
+    distance = 2,
+    objYaw = 160.00022888184,
+    locked = true,
+}
+
+-- 6 created by luap
+Config.DoorList['pacificbank-6'] = {
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    objCoords = vec3(277.595306, 223.541580, 110.279175),
+    objName = 267980221,
+    doorLabel = 'stairs 2',
+    doorRate = 1.0,
+    pickable = true,
+    fixText = false,
+    distance = 2,
+    objYaw = 160.00022888184,
+    locked = true,
+}
+
+-- 7 created by luap
+Config.DoorList['pacificbank-7'] = {
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    objCoords = vec3(244.558014, 216.897278, 97.317978),
+    objName = 409280169,
+    doorLabel = 'thermite 1',
+    doorRate = 1.0,
+    fixText = false,
+    distance = 2,
+    objYaw = 340.00024414062,
+    locked = true,
+}
+
+-- 8 created by luap
+Config.DoorList['pacificbank-8'] = {
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    objCoords = vec3(250.564209, 233.399384, 97.317978),
+    objName = 409280169,
+    doorLabel = 'thermite 2',
+    doorRate = 1.0,
+    fixText = false,
+    distance = 2,
+    objYaw = 340.00024414062,
+    locked = true,
+}
