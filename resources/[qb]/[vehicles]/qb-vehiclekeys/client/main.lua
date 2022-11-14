ce7bb506cc46a3a2b1f40197e6c1ef577eb0cca5
luap710 <92261388+itsluap@users.noreply.github.com>
2022-11-13 21:22:46 -0600

    Update main.lua

--- resources/[qb]/[vehicles]/qb-vehiclekeys/client/main.lua
+++ resources/[qb]/[vehicles]/qb-vehiclekeys/client/main.lua
@@ -555,7 +555,7 @@ function AttemptPoliceAlert(type)
             chance = Config.PoliceNightAlertChance
         end
         if math.random() <= chance then
-           TriggerServerEvent('police:server:policeAlert', Lang:t("info.palert") .. type)
+            exports['ps-dispatch']:CarJacking(vehicle)
         end
         AlertSend = true
         SetTimeout(Config.AlertCooldown, function()
