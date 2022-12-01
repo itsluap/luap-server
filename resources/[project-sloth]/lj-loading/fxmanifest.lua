bf3c08914947119dc6abec63514938e4996cd478
itsluap <92261388+itsluap@users.noreply.github.com>
2022-12-01 00:26:46 -0600

    .

---
+++ resources/[project-sloth]/lj-loading/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+description 'lj-loading'
+version '1.3'
+author 'github.com/loljoshie'
+
+files {
+	'ui.html',
+	'html/*',
+	'assets/*',
+}
+
+loadscreen {
+	'ui.html'
+}
+
+loadscreen_manual_shutdown 'yes'
