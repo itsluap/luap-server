25033e1967bcf5fab32a6ae8ac1caaaf39c7d5b6
luap710 <92261388+itsluap@users.noreply.github.com>
2022-12-08 19:37:11 -0600

    Update config.lua

--- resources/[project-sloth]/ps-multijob/config.lua
+++ resources/[project-sloth]/ps-multijob/config.lua
@@ -13,6 +13,13 @@ Config.WhitelistJobs = {
 	["police"] = true,
 	["ambulance"] = true,
 	["mechanic"] = true,
+	["cardealer"] = true,
+	["realestate"] = true,
+	["ottos"] = true,
+	["tuner"] = true,
+	["autoex"] = true,
+	["burgershot"] = true,
+	["vanilla"] = true,
 	["judge"] = true,
 	["lawyer"] = true,
 }
@@ -20,12 +27,17 @@ Config.WhitelistJobs = {
 Config.Descriptions = {
 	["police"] = "Shoot some criminals or maybe be a good cop and arrest them",
 	["ambulance"] = "Fix the bullet holes",
-	["mechanic"] = "Fix the bullet holes",
+	["mechanic"] = "Fix and customize cars",
+	["ottos"] = "Fix and customize cars",
+	["tuner"] = "Fix and customize cars",
+	["autoex"] = "Fix and customize cars",
 	["tow"] = "Pickup the tow truck and steal some vehicles",
 	["taxi"] = "You should retire, this job sucks",
 	["bus"] = "You should retire, this job sucks",
 	["realestate"] = "Sell houses or something",
 	["cardealer"] = "Sell cars or something",
+	["burgershot"] = "Sell burgers or something",
+	["vanilla"] = "Shawty I don't mind",
 	["judge"] = "Decide if people are guilty",
 	["lawyer"] = "Help the good or the bad",
 	["reporter"] = "Lowkey useless",
@@ -35,3 +47,4 @@ Config.Descriptions = {
 	["hotdog"] = "Sell them glizzys",
 	["unemployed"] = "You should probably get a job",
 }
+
