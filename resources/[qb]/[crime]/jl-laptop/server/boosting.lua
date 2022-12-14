50f895fad0f0ee78247e140053d9d97d222983f2
luap710 <92261388+itsluap@users.noreply.github.com>
2022-12-13 23:47:52 -0600

    Update boosting.lua

--- resources/[qb]/[crime]/jl-laptop/server/boosting.lua
+++ resources/[qb]/[crime]/jl-laptop/server/boosting.lua
@@ -832,7 +832,7 @@ CreateThread(function()
                         end
                         -- If skipped is bigger or equal to 25 we give player a contract for waiting
                         -- Otherwise we say if they been in queue longer than 2-7 skips and their chance is higher than 0.75 (meaning 25% chance) we will reward them with a contract
-                        if v.skipped >= 25 --[[or (v.skipped >= math.random(2, 7) and ContractChance >= 0.75)--]] then
+                        if v.skipped == 25 --[[or (v.skipped >= math.random(2, 7) and ContractChance >= 0.75)--]] then
                             generateContract(v.src)
                         else
                             v.skipped += 1
