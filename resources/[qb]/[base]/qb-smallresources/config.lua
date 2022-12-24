aaea3cac7b4faac33a4621c99581ec6f61f6fc96
luap710 <92261388+itsluap@users.noreply.github.com>
2022-12-23 22:56:14 -0600

    Update config.lua

--- resources/[qb]/[base]/qb-smallresources/config.lua
+++ resources/[qb]/[base]/qb-smallresources/config.lua
@@ -13,7 +13,7 @@ Config.DirtLevel = 0.1 -- Threshold for the dirt level to be counted as dirty
 Config.DisableAmbience = true -- Disabled distance sirens, distance car alarms, etc
 
 Config.Disable = {
-    disableHudComponents = {}, -- Hud Components: https://docs.fivem.net/natives/?_0x6806C51AD12B83B8 -- 14 is crosshair
+    disableHudComponents = {1, 2, 3, 4, 6, 8, 7, 9, 13, 14, 19, 20, 21}, -- Hud Components: https://docs.fivem.net/natives/?_0x6806C51AD12B83B8 -- 14 is crosshair
     disableControls = {37}, -- Controls: https://docs.fivem.net/docs/game-references/controls/
     displayAmmo = true -- false disables ammo display
 }
