038804fab2dd84450b02a911d792a732fb829511
luap710 <92261388+itsluap@users.noreply.github.com>
2022-12-28 17:33:36 -0600

    .

--- resources/[qb]/[ui]/qb-doorlock/configs/ottos.lua
+++ resources/[qb]/[ui]/qb-doorlock/configs/ottos.lua
@@ -1,267 +1,76 @@
 
 
--- ottos - garagedoor left
-table.insert(Config.DoorList, {
-	fixText = true,
-	objHash = 270330101,
-	slides = false,
-	authorizedJobs = { ['ottos']=0 },
-	maxDistance = 2.0,
-	garage = true,
-	objHeading = 270.00012207031,
-	lockpick = false,
-	locked = true,
-	objCoords = vector3(823.839, -805.582, 27.39546),
-	audioRemote = false,		
-	-- oldMethod = true,
-	-- audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
-	-- audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
-	-- autoLock = 1000
-})
+-- 1 created by luap
+Config.DoorList['ottos-1'] = {
+    fixText = false,
+    objCoords = vec3(800.391479, -824.354553, 26.484516),
+    doorLabel = 'cardealer_front',
+    locked = true,
+    authorizedJobs = { ['ottos'] = 0 },
+    distance = 2,
+    doorRate = 1.0,
+    objName = -147325430,
+    authorizedGangs = { ['cartel'] = 0 },
+    objYaw = 0.0,
+    doorType = 'door',
+}
 
--- ottos - garagedoor middle
-table.insert(Config.DoorList, {
-	fixText = true,
-	objHash = 270330101,
-	slides = false,
-	authorizedJobs = { ['ottos']=0 },
-	maxDistance = 2.0,
-	garage = true,
-	objHeading = 270.00012207031,
-	lockpick = false,
-	locked = true,
-	objCoords = vector3(823.839, -812.9502, 27.39546),
-	audioRemote = false,		
-	-- oldMethod = true,
-	-- audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
-	-- audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
-	-- autoLock = 1000
-})
+-- 2 created by luap
+Config.DoorList['ottos-2'] = {
+    fixText = false,
+    objCoords = vec3(823.761169, -828.994812, 26.482376),
+    doorLabel = 'mech_front',
+    locked = true,
+    authorizedJobs = { ['ottos'] = 0 },
+    distance = 2,
+    doorRate = 1.0,
+    objName = -147325430,
+    authorizedGangs = { ['cartel'] = 0 },
+    objYaw = 90.0,
+    doorType = 'door',
+}
 
--- ottos - garagedoor right
-table.insert(Config.DoorList, {
-	fixText = true,
-	objHash = 270330101,
-	slides = false,
-	authorizedJobs = { ['ottos']=0 },
-	maxDistance = 2.0,
-	garage = true,
-	objHeading = 270.00012207031,
-	lockpick = false,
-	locked = true,
-	objCoords = vector3(823.8391, -820.271, 27.39546),
-	audioRemote = false,		
-	-- oldMethod = true,
-	-- audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
-	-- audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
-	-- autoLock = 1000
-})
+-- 3 created by luap
+Config.DoorList['ottos-3'] = {
+    fixText = false,
+    objCoords = vec3(823.839050, -820.270996, 27.395458),
+    doorLabel = 'mech_garage1',
+    locked = true,
+    authorizedJobs = { ['ottos'] = 0 },
+    distance = 4,
+    doorRate = 1.0,
+    objName = 270330101,
+    authorizedGangs = { ['cartel'] = 0 },
+    objYaw = 270.00012207031,
+    doorType = 'garage',
+}
 
--- ottos - main entrance
-table.insert(Config.DoorList, {
-	fixText = false,
-	objHash = -147325430,
-	slides = false,
-	authorizedJobs = { ['ottos']=0 },
-	maxDistance = 2.0,
-	garage = false,
-	objHeading = 90.0,
-	lockpick = false,
-	locked = true,
-	objCoords = vector3(823.7612, -828.9948, 26.48238),
-	audioRemote = false,		
-	-- oldMethod = true,
-	-- audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
-	-- audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
-	-- autoLock = 1000
-})
+-- 4 created by luap
+Config.DoorList['ottos-4'] = {
+    fixText = false,
+    objCoords = vec3(823.838989, -812.950195, 27.395458),
+    doorLabel = 'mech_garage2',
+    locked = true,
+    authorizedJobs = { ['ottos'] = 0 },
+    distance = 4,
+    doorRate = 1.0,
+    objName = 270330101,
+    authorizedGangs = { ['cartel'] = 0 },
+    objYaw = 270.00012207031,
+    doorType = 'garage',
+}
 
--- ottos - lobby to office
-table.insert(Config.DoorList, {
-	fixText = false,
-	objHash = -147325430,
-	slides = false,
-	authorizedJobs = { ['ottos']=0 },
-	maxDistance = 2.0,
-	garage = false,
-	objHeading = 90.0,
-	lockpick = false,
-	locked = true,
-	objCoords = vector3(830.6199, -829.0284, 26.48238),
-	audioRemote = false,		
-	-- oldMethod = true,
-	-- audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
-	-- audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
-	-- autoLock = 1000
-})
-
--- ottos - lobby to garage
-table.insert(Config.DoorList, {
-	fixText = false,
-	objHash = -147325430,
-	slides = false,
-	authorizedJobs = { ['ottos']=0 },
-	maxDistance = 2.0,
-	garage = false,
-	objHeading = 180.00003051758,
-	lockpick = false,
-	locked = true,
-	objCoords = vector3(829.9662, -824.6747, 26.48238),
-	audioRemote = false,		
-	-- oldMethod = true,
-	-- audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
-	-- audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
-	-- autoLock = 1000
-})
-
--- ottos - garage to locker
-table.insert(Config.DoorList, {
-	fixText = false,
-	objHash = -147325430,
-	slides = false,
-	authorizedJobs = { ['ottos']=0 },
-	maxDistance = 2.0,
-	garage = false,
-	objHeading = 270.00003051758,
-	lockpick = false,
-	locked = true,
-	objCoords = vector3(837.6531, -821.3135, 26.48238),
-	audioRemote = false,		
-	-- oldMethod = true,
-	-- audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
-	-- audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
-	-- autoLock = 1000
-})
-
--- ottos - back entrance
-table.insert(Config.DoorList, {
-	fixText = false,
-	objHash = 263193286,
-	slides = false,
-	authorizedJobs = { ['ottos']=0 },
-	maxDistance = 2.0,
-	garage = false,
-	objHeading = 0.0,
-	lockpick = false,
-	locked = true,
-	objCoords = vector3(840.5321, -820.5624, 26.48237),
-	audioRemote = false,		
-	-- oldMethod = true,
-	-- audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
-	-- audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
-	-- autoLock = 1000
-})
-
--- ottos - cardealer entrance
-table.insert(Config.DoorList, {
-	fixText = false,
-	objHash = -147325430,
-	slides = false,
-	authorizedJobs = { ['ottos']=0 },
-	maxDistance = 2.0,
-	garage = false,
-	objHeading = 0.0,
-	lockpick = false,
-	locked = true,
-	objCoords = vector3(800.3915, -824.3546, 26.48452),
-	audioRemote = false,		
-	-- oldMethod = true,
-	-- audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
-	-- audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
-	-- autoLock = 1000
-})
-
--- ottos - cardealer locker
-table.insert(Config.DoorList, {
-	fixText = false,
-	objHash = -147325430,
-	slides = false,
-	authorizedJobs = { ['ottos']=0 },
-	maxDistance = 2.0,
-	garage = false,
-	objHeading = 180.00019836426,
-	lockpick = false,
-	locked = true,
-	objCoords = vector3(802.2042, -829.1265, 26.48452),
-	audioRemote = false,		
-	-- oldMethod = true,
-	-- audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
-	-- audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
-	-- autoLock = 1000
-})
-
--- ottos - cardealer office
-table.insert(Config.DoorList, {
-	fixText = false,
-	objHash = -147325430,
-	slides = false,
-	authorizedJobs = { ['ottos']=0 },
-	maxDistance = 2.0,
-	garage = false,
-	objHeading = 270.00012207031,
-	lockpick = false,
-	locked = true,
-	objCoords = vector3(800.09, -827.1212, 26.48452),
-	audioRemote = false,		
-	-- oldMethod = true,
-	-- audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
-	-- audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
-	-- autoLock = 1000
-})
-
--- ottos - 247 front entrance right
-table.insert(Config.DoorList, {
-	fixText = false,
-	objHash = 997554217,
-	slides = false,
-	authorizedJobs = { ['ottos']=0 },
-	maxDistance = 2.0,
-	garage = false,
-	objHeading = 359.87005615234,
-	lockpick = false,
-	locked = true,
-	objCoords = vector3(818.3854, -784.4561, 26.32103),
-	audioRemote = false,		
-	-- oldMethod = true,
-	-- audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
-	-- audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
-	-- autoLock = 1000
-})
-
--- ottos - 247 front entrance left
-table.insert(Config.DoorList, {
-	fixText = false,
-	objHash = 1196685123,
-	slides = false,
-	authorizedJobs = { ['ottos']=0 },
-	maxDistance = 2.0,
-	garage = false,
-	objHeading = 179.87001037598,
-	lockpick = false,
-	locked = true,
-	objCoords = vector3(815.7849, -784.4501, 26.32103),
-	audioRemote = false,		
-	-- oldMethod = true,
-	-- audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
-	-- audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
-	-- autoLock = 1000
-})
-
--- ottos - 247 backroom
-table.insert(Config.DoorList, {
-	fixText = false,
-	objHash = 1266035946,
-	slides = false,
-	authorizedJobs = { ['ottos']=0 },
-	maxDistance = 2.0,
-	garage = false,
-	objHeading = 359.87005615234,
-	lockpick = false,
-	locked = true,
-	objCoords = vector3(812.252, -777.1773, 26.32631),
-	audioRemote = false,		
-	-- oldMethod = true,
-	-- audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
-	-- audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
-	-- autoLock = 1000
-})
+-- 5 created by luap
+Config.DoorList['ottos-5'] = {
+    fixText = false,
+    objCoords = vec3(823.838989, -805.582031, 27.395458),
+    doorLabel = 'mech_garage3',
+    locked = true,
+    authorizedJobs = { ['ottos'] = 0 },
+    distance = 4,
+    doorRate = 1.0,
+    objName = 270330101,
+    authorizedGangs = { ['cartel'] = 0 },
+    objYaw = 270.00012207031,
+    doorType = 'garage',
+}
