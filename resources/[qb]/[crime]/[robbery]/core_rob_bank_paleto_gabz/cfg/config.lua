0fc189daae67355923492d2e22098a996fe52d12
luap710 <92261388+itsluap@users.noreply.github.com>
2022-12-29 22:23:14 -0600

    Update config.lua

--- resources/[qb]/[crime]/[robbery]/core_rob_bank_paleto_gabz/cfg/config.lua
+++ resources/[qb]/[crime]/[robbery]/core_rob_bank_paleto_gabz/cfg/config.lua
@@ -93,7 +93,7 @@ cfg.robbery = {
                     type = "hackvoltage_01",
     
                     minigame = {
-                        event = "CORE_ROB_MINIGAMES:VoltLab_c", --event
+                        event = "CORE_ROB_BANK_PACIFIC:HACKMINIGAMECUSTOM_A", --event
                         data = { -- data that will pass to the event
                             timer = 25, -- seconds
                         },
@@ -144,12 +144,12 @@ cfg.robbery = {
                     type = "hacksecuritypanel_01",
     
                     item_needed = {
-                        {"robbery_card_01",1,100}, -- {item_name,item_amount,chance_to_remove}
-                        {"robbery_laptop_01",1,100}, -- {item_name,item_amount,chance_to_remove}
+                        {"security_card_01",1,100}, -- {item_name,item_amount,chance_to_remove}
+                        {"pixellaptop",1,0}, -- {item_name,item_amount,chance_to_remove}
                     },
     
                     minigame = {
-                        event = "CORE_ROB_MINIGAMES:TerminalMinigame_c",
+                        event = "CORE_ROB_BANK_PACIFIC:HACKMINIGAMECUSTOM_B",
                         data = {
                             background = 4, -- 0 to 6 | MUST BE INTEGER
                             lives = 5.0, -- max is 7.0 | MUST BE FLOAT
