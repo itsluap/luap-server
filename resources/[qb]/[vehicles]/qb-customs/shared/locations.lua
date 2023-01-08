84c73cd44f550e95407680cdfe4f10d8a26247b7
luap710 <92261388+itsluap@users.noreply.github.com>
2023-01-08 01:18:00 -0600

    Update locations.lua

--- resources/[qb]/[vehicles]/qb-customs/shared/locations.lua
+++ resources/[qb]/[vehicles]/qb-customs/shared/locations.lua
@@ -50,4 +50,45 @@ Config.Locations = {
             { coords = vector3(135.92, -3030.5, 7.04), length = 6.0, width = 4.0, heading = 180.0, minZ = 6.0, maxZ = 10.0 },
         }
     },
+    ['LSC'] = {
+        settings = {
+            label = 'Los Santos Customs',
+            welcomeLabel = "Welcome to the Los Santos Customs!",
+            enabled = true,
+        },
+        blip = {
+            label = 'Los Santos Customs',
+            coords = vector3(-337.6, -136.7, 39.01),
+            sprite = 446,
+            scale = 0.65,
+            color = 0,
+            display = 4,
+            enabled = false,
+        },
+        categories = {
+            mods = true,
+            turbo = true,
+            repair = true,
+            respray = true,
+            liveries = true,
+            wheels = true,
+            tint = true,
+            plate = true,
+            extras = true,
+            neons = true,
+            xenons = true,
+            horn = true,
+            cosmetics = true,
+        },
+        drawtextui = {
+            text = "Los Santos Customs",
+            icon = "material-icons",
+            materialIcon = 'construction',
+        },
+        restrictions = { deniedClasses = { 18 } },
+        zones = {
+            { coords = vector3(-337.6, -136.7, 39.01), length = 6.0, width = 4.0, heading = 180.0, minZ = 6.0, maxZ = 10.0 },
+            --{ coords = vector3(135.92, -3030.5, 7.04), length = 6.0, width = 4.0, heading = 180.0, minZ = 6.0, maxZ = 10.0 },
+        }
+    },
 }
