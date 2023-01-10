d8eed7a1a5cc70df0c708df6bc4dca9ecc2eb941
luap710 <92261388+itsluap@users.noreply.github.com>
2023-01-09 21:35:58 -0600

    Update main.lua

--- resources/[qb]/[phone]/qb-crypto/client/main.lua
+++ resources/[qb]/[phone]/qb-crypto/client/main.lua
@@ -143,18 +143,20 @@ CreateThread(function()
 						TriggerEvent('inventory:client:requiredItems', requiredItems2, false)
 					end
 				end
-			elseif pacificdist3 < 15 then
-				print('pacific dist3')
-				sleep = 5
-				if pacificdist3 < 1.5 then
-					if not requiredItemsShowed then
-						requiredItemsShowed = true
-						TriggerEvent('inventory:client:requiredItems', requiredItems2, true)
-					end
-				else
-					if requiredItemsShowed then
-						requiredItemsShowed = false
-						TriggerEvent('inventory:client:requiredItems', requiredItems2, false)
+			else
+				if pacificdist3 < 15 then
+					print('pacific dist3')
+					sleep = 5
+					if pacificdist3 < 1.5 then
+						if not requiredItemsShowed then
+							requiredItemsShowed = true
+							TriggerEvent('inventory:client:requiredItems', requiredItems2, true)
+						end
+					else
+						if requiredItemsShowed then
+							requiredItemsShowed = false
+							TriggerEvent('inventory:client:requiredItems', requiredItems2, false)
+						end
 					end
 				end
 			end
