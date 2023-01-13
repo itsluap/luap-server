af1baf5745bd02f30675eff298c9553e330f9d3b
luap710 <92261388+itsluap@users.noreply.github.com>
2023-01-12 20:56:09 -0600

    Update client.lua

--- resources/[qb]/[crime]/qb-moneywash/client.lua
+++ resources/[qb]/[crime]/qb-moneywash/client.lua
@@ -72,12 +72,15 @@ end)
 
 RegisterNetEvent("laundry:openwasher")
 AddEventHandler("laundry:openwasher", function(data)
+    local c = false 
     QBCore.Functions.TriggerCallback("laundry:isWashing", function(result)
-        if result == false then
-            TriggerServerEvent("inventory:server:OpenInventory", "stash", "washer"..data.id, {maxweight = 1500000, slots = 10})
-            TriggerEvent("inventory:client:SetCurrentStash", "washer"..data.id)
-        else 
-            print('money wash fail notification here') print('moneywash is still running')
-        end
-    end)
+        c = result
+    end, k)
+    Wait(200)
+    if not c then
+        TriggerServerEvent("inventory:server:OpenInventory", "stash", "washer"..data.id, {maxweight = 1500000, slots = 10})
+        TriggerEvent("inventory:client:SetCurrentStash", "washer"..data.id)
+    else 
+        print('money wash fail notification here') print('moneywash is still running')
+    end
 end)
