51cecd489e3b9847dec4dcf16f2c877f2ee5104f
itsluap <92261388+itsluap@users.noreply.github.com>
2023-01-25 02:00:34 -0600

    Create syndicate.lua

---
+++ resources/[qb]/[ui]/qb-doorlock/configs/syndicate.lua
@@ -0,0 +1,29 @@
+
+
+-- 1 created by luap
+Config.DoorList['syndicate-1'] = {
+    doorType = 'sliding',
+    objYaw = 89.999977111816,
+    authorizedGangs = { ['syndicate'] = 0 },
+    doorLabel = 'front gate',
+    locked = true,
+    fixText = false,
+    objCoords = vec3(-666.879028, -889.523010, 23.531740),
+    distance = 3,
+    objName = -1603817716,
+    doorRate = 1.0,
+}
+
+-- 2 created by luap
+Config.DoorList['syndicate-2'] = {
+    doorType = 'garage',
+    objYaw = 270.00003051758,
+    authorizedGangs = { ['syndicate'] = 0 },
+    doorLabel = 'inside garage',
+    locked = true,
+    fixText = false,
+    objCoords = vec3(-675.330994, -878.722168, 25.238443),
+    distance = 3,
+    objName = 245838764,
+    doorRate = 1.0,
+}
