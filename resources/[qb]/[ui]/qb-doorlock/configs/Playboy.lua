04cda1bc494abda40e4aa0136da8be8a38c8ca67
itsluap <92261388+itsluap@users.noreply.github.com>
2023-01-25 18:47:02 -0600

    remove redline

---
+++ resources/[qb]/[ui]/qb-doorlock/configs/Playboy.lua
@@ -0,0 +1,44 @@
+
+
+
+-- 1 created by Twitch-N0pe121
+Config.DoorList['Playboy-1'] = {
+    distance = 4,
+    authorizedCitizenIDs = { ['NDL17225'] = true },
+    objCoords = vec3(-1524.024048, 78.848412, 57.321274),
+    locked = true,
+    doorRate = 1.0,
+    doorLabel = 'Garage Left',
+    objYaw = 187.71682739258,
+    objName = -112388600,
+    fixText = false,
+    doorType = 'garage',
+}
+
+-- 2 created by Twitch-N0pe121
+Config.DoorList['Playboy-2'] = {
+    distance = 2,
+    doorLabel = 'Office Double',
+    doorType = 'double',
+    doors = {
+        {objName = 53021951, objYaw = 225.00003051758, objCoords = vec3(-1527.849243, 143.917770, 60.942554)},
+        {objName = 53021951, objYaw = 44.999973297119, objCoords = vec3(-1526.418457, 145.348602, 60.942554)}
+    },
+    doorRate = 1.0,
+    authorizedCitizenIDs = { ['NDL17225'] = true },
+    locked = true,
+}
+
+-- 3 created by Twitch-N0pe121
+Config.DoorList['Playboy-3'] = {
+    distance = 2,
+    authorizedCitizenIDs = { ['NDL17225'] = true },
+    objCoords = vec3(-1535.106201, 142.694672, 60.940239),
+    locked = true,
+    doorRate = 1.0,
+    doorLabel = 'Office Single',
+    objYaw = 315.0,
+    objName = 116626580,
+    fixText = false,
+    doorType = 'door',
+}
