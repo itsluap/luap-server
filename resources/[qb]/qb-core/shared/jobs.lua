a04d22ceb582bf75aad288992c12d7581f11fa93
luap710 <92261388+itsluap@users.noreply.github.com>
2023-01-25 19:52:46 -0600

    .

--- resources/[qb]/qb-core/shared/jobs.lua
+++ resources/[qb]/qb-core/shared/jobs.lua
@@ -340,31 +340,16 @@ QBShared.Jobs = {
             },
         },
 	},
-    ["burgershot"] = {
-		label = "Burgershot Employee",
+    ['burgershot'] = {
+		label = 'BurgerShot',
 		defaultDuty = true,
+		offDutyPay = false,
 		grades = {
-            ['0'] = {
-                name = "Trainee",
-                payment = 50
-            },
-			['1'] = {
-                name = "Employee",
-                payment = 75
-            },
-			['2'] = {
-                name = "Burger Flipper",
-                payment = 100
-            },
-			['3'] = {
-                name = "Manager",
-                payment = 125
-            },
-			['4'] = {
-                name = "Owner",
-				isboss = true,
-                payment = 150
-            },
+            ['0'] = { name = 'Recruit', payment = 50 },
+			['1'] = { name = 'Novice', payment = 75 },
+			['2'] = { name = 'Experienced', payment = 100 },
+			['3'] = { name = 'Advanced', payment = 125 },
+			['4'] = { name = 'Manager', isboss = true, payment = 150 },
         },
 	},
 	['judge'] = {
