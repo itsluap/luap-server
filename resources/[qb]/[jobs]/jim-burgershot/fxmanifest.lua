5c4135fc62a620c441c10ea07b5dd92f9e596c52
luap710 <92261388+itsluap@users.noreply.github.com>
2023-01-25 19:39:11 -0600

    new burgershot

---
+++ resources/[qb]/[jobs]/jim-burgershot/fxmanifest.lua
@@ -0,0 +1,14 @@
+name "Jim-BurgerShot"
+author "Jimathy"
+version "v2.3.7"
+description "BurgerShot Script By Jimathy"
+fx_version "cerulean"
+game "gta5"
+lua54 'yes'
+
+shared_scripts { 'config.lua', 'locales/*.lua',	'shared/*.lua' }
+server_scripts { 'server/*.lua' }
+client_scripts { '@PolyZone/client.lua', '@PolyZone/BoxZone.lua', '@PolyZone/EntityZone.lua', '@PolyZone/CircleZone.lua', '@PolyZone/ComboZone.lua', 'client/*.lua' }
+
+escrow_ignore { '*.lua', 'client/*.lua', 'server/*.lua', 'locales/*.lua', }
+dependency '/assetpacks'
