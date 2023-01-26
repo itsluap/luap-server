d8c5f635d8dffa5190cd8ffea2de37dfa8ca2bd2
itsluap <92261388+itsluap@users.noreply.github.com>
2023-01-25 21:40:09 -0600

    Create cleanmanor.lua

---
+++ resources/[qb]/[ui]/qb-doorlock/configs/cleanmanor.lua
@@ -0,0 +1,99 @@
+
+
+-- 1 created by luap
+Config.DoorList['cleanmanor-1'] = {
+    doors = {
+        {objName = 1558415341, objYaw = 179.98397827148, objCoords = vec3(-1805.542847, 436.221375, 129.227142)},
+        {objName = 1558415341, objYaw = 0.0, objCoords = vec3(-1804.333618, 436.221039, 129.227142)}
+    },
+    doorRate = 1.0,
+    distance = 2,
+    doorLabel = 'courtyard entrance',
+    locked = true,
+    doorType = 'double',
+    authorizedCitizenIDs = { ['AVE63694'] = true },
+}
+
+-- 2 created by luap
+Config.DoorList['cleanmanor-2'] = {
+    doors = {
+        {objName = 1100960097, objYaw = 89.999977111816, objCoords = vec3(-1813.162720, 447.971191, 128.728775)},
+        {objName = 1100960097, objYaw = 270.00003051758, objCoords = vec3(-1813.162720, 445.963318, 128.728775)}
+    },
+    doorRate = 1.0,
+    distance = 2,
+    doorLabel = 'courtyard entrance right',
+    locked = true,
+    doorType = 'double',
+    authorizedCitizenIDs = { ['AVE63694'] = true },
+}
+
+-- 3 created by luap
+Config.DoorList['cleanmanor-3'] = {
+    doors = {
+        {objName = 1314286287, objYaw = 0.0, objCoords = vec3(-1814.443726, 423.646362, 128.701004)},
+        {objName = 1314286287, objYaw = 180.00001525879, objCoords = vec3(-1817.443115, 423.646362, 128.701004)}
+    },
+    doorRate = 1.0,
+    distance = 2,
+    doorLabel = 'back entrance (by pool table)',
+    locked = true,
+    doorType = 'double',
+    authorizedCitizenIDs = { ['AVE63694'] = true },
+}
+
+-- 4 created by luap
+Config.DoorList['cleanmanor-4'] = {
+    doors = {
+        {objName = -627047580, objYaw = 1.0017911336035e-05, objCoords = vec3(-1804.171387, 428.940430, 129.135056)},
+        {objName = -627047580, objYaw = 179.98397827148, objCoords = vec3(-1805.386719, 428.940765, 129.135056)}
+    },
+    doorRate = 1.0,
+    distance = 2,
+    doorLabel = 'back entrance main',
+    locked = true,
+    doorType = 'double',
+    authorizedCitizenIDs = { ['AVE63694'] = true },
+}
+
+-- 5 created by luap
+Config.DoorList['cleanmanor-5'] = {
+    doors = {
+        {objName = 1100960097, objYaw = 0.0, objCoords = vec3(-1815.435303, 428.556000, 132.610245)},
+        {objName = 1100960097, objYaw = 180.00001525879, objCoords = vec3(-1817.442139, 428.556000, 132.610245)}
+    },
+    doorRate = 1.0,
+    distance = 2,
+    doorLabel = 'back entrance top',
+    locked = true,
+    doorType = 'double',
+    authorizedCitizenIDs = { ['AVE63694'] = true },
+}
+
+-- 6 created by luap
+Config.DoorList['cleanmanor-6'] = {
+    doors = {
+        {objName = 1100960097, objYaw = 89.983963012695, objCoords = vec3(-1788.571777, 435.876007, 128.488068)},
+        {objName = 1100960097, objYaw = 269.98400878906, objCoords = vec3(-1788.572266, 433.871033, 128.488068)}
+    },
+    doorRate = 1.0,
+    distance = 2,
+    doorLabel = 'side entrance',
+    locked = true,
+    doorType = 'double',
+    authorizedCitizenIDs = { ['AVE63694'] = true },
+}
+
+-- 7 created by luap
+Config.DoorList['cleanmanor-7'] = {
+    objCoords = vec3(-1786.580811, 463.334503, 128.572266),
+    objYaw = 285.0,
+    fixText = false,
+    authorizedCitizenIDs = { ['AVE63694'] = true },
+    doorRate = 1.0,
+    distance = 2,
+    doorType = 'garage',
+    locked = true,
+    objName = 1051798911,
+    doorLabel = 'garage left',
+}
