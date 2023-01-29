d7964e86d5fae9ec40a253098a79ce90283621c3
itsluap <92261388+itsluap@users.noreply.github.com>
2023-01-28 20:34:05 -0600

    add qb-arcade

---
+++ resources/[qb]/[ui]/qb-doorlock/configs/puffpuffpass.lua
@@ -0,0 +1,46 @@
+
+
+-- 1 created by luap
+Config.DoorList['puffpuffpass-1'] = {
+    doorLabel = 'lower entrance',
+    objName = 199942448,
+    fixText = false,
+    locked = true,
+    doorRate = 1.0,
+    doorType = 'door',
+    objCoords = vec3(-1550.177246, -435.987305, 36.133076),
+    objYaw = 229.71426391602,
+    distance = 2,
+    authorizedJobs = { ['puffpuffpass'] = 0 },
+    authorizedCitizenIDs = { ['AVE63694'] = true },
+}
+
+-- 2 created by luap
+Config.DoorList['puffpuffpass-2'] = {
+    doorLabel = 'upper entrance',
+    objName = 199942448,
+    fixText = false,
+    locked = true,
+    doorRate = 1.0,
+    doorType = 'door',
+    objCoords = vec3(-1552.651489, -439.144135, 40.769726),
+    objYaw = 49.714244842529,
+    distance = 2,
+    authorizedJobs = { ['puffpuffpass'] = 0 },
+    authorizedCitizenIDs = { ['AVE63694'] = true },
+}
+
+-- 3 created by luap
+Config.DoorList['puffpuffpass-3'] = {
+    doorLabel = 'to hallway',
+    objName = 1075032193,
+    fixText = false,
+    locked = true,
+    doorRate = 1.0,
+    doorType = 'door',
+    objCoords = vec3(-1562.624878, -442.500031, 36.037617),
+    objYaw = 319.71429443359,
+    distance = 2,
+    authorizedJobs = { ['puffpuffpass'] = 0 },
+    authorizedCitizenIDs = { ['AVE63694'] = true },
+}
