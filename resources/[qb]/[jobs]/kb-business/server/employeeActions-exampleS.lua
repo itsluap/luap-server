692d2871d2bea0cef0d6a053cedbe1e9dfebdec8
luap710 <92261388+itsluap@users.noreply.github.com>
2023-01-29 01:50:22 -0600

    Update employeeActions-exampleS.lua

--- resources/[qb]/[jobs]/kb-business/server/employeeActions-exampleS.lua
+++ resources/[qb]/[jobs]/kb-business/server/employeeActions-exampleS.lua
@@ -73,10 +73,10 @@ RegisterNetEvent('kb-business:server:toggleItem', function(give, item, amount, n
 				if Config.Debug then print("^5Debug^7: ^1Removing ^2from Player^7(^2"..src.."^7) '^6"..QBCore.Shared.Items[item].label.."^7(^2x^6"..(amount or "1").."^7)'") end
 				TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[item], "remove", amount)
 			end
-			print(GlobalState.canMakeWeed)
+			--print(GlobalState.canMakeWeed)
 		end
 	elseif give then
-		print(GlobalState.canMakeWeed)
+		--print(GlobalState.canMakeWeed)
 		if GlobalState.canMakeWeed then
 			if QBCore.Functions.GetPlayer(src).Functions.AddItem(item, amount) then
 				if Config.Debug then print("^5Debug^7: ^4Giving ^2Player^7(^2"..src.."^7) '^6"..QBCore.Shared.Items[item].label.."^7(^2x^6"..(amount or "1").."^7)'") end
