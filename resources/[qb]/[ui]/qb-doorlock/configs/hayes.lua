83e1c428647301a58e12a20119b905cce6dc44f2
itsluap <92261388+itsluap@users.noreply.github.com>
2023-02-01 11:23:10 -0600

    Create hayes.lua

---
+++ resources/[qb]/[ui]/qb-doorlock/configs/hayes.lua
@@ -0,0 +1,57 @@
+
+
+-- 1 created by luap
+Config.DoorList['hayes-1'] = {
+    locked = true,
+    authorizedJobs = { ['hayes'] = 0 },
+    doorType = 'door',
+    objYaw = 31.999956130981,
+    fixText = false,
+    doorLabel = 'entrance door',
+    distance = 2,
+    objCoords = vec3(-1434.155273, -448.586090, 36.059235),
+    objName = -634936098,
+    doorRate = 1.0,
+}
+
+-- 2 created by luap
+Config.DoorList['hayes-2'] = {
+    locked = true,
+    authorizedJobs = { ['hayes'] = 0 },
+    doorType = 'garage',
+    objYaw = 32.001628875732,
+    fixText = false,
+    doorLabel = 'garage right',
+    distance = 3,
+    objCoords = vec3(-1427.326294, -444.151581, 34.773518),
+    objName = 1715394473,
+    doorRate = 1.0,
+}
+
+-- 3 created by luap
+Config.DoorList['hayes-3'] = {
+    locked = true,
+    authorizedJobs = { ['hayes'] = 0 },
+    doorType = 'garage',
+    objYaw = 31.999959945679,
+    fixText = false,
+    doorLabel = 'garage center',
+    distance = 3,
+    objCoords = vec3(-1421.117798, -440.272064, 34.773518),
+    objName = 1715394473,
+    doorRate = 1.0,
+}
+
+-- 4 created by luap
+Config.DoorList['hayes-4'] = {
+    locked = true,
+    authorizedJobs = { ['hayes'] = 0 },
+    doorType = 'garage',
+    objYaw = 31.999959945679,
+    fixText = false,
+    doorLabel = 'garage left',
+    distance = 3,
+    objCoords = vec3(-1414.869141, -436.367523, 34.773518),
+    objName = 1715394473,
+    doorRate = 1.0,
+}
