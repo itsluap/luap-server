ccc03be4b6f9640275f86e0f5cc49d5d8f37c529
luap710 <92261388+itsluap@users.noreply.github.com>
2023-10-30 13:35:24 -0500

    Update player.lua

--- resources/[qb]/qb-core/server/player.lua
+++ resources/[qb]/qb-core/server/player.lua
@@ -535,7 +535,7 @@ end
 local playertables = { -- Add tables as needed
     { table = 'players' },
     { table = 'apartments' },
-    { table = 'bank_accounts_new' },
+    -- { table = 'bank_accounts_new' }, -- not working, no CID in table
     { table = 'crypto_transactions' },
     { table = 'phone_invoices' },
     { table = 'phone_messages' },
