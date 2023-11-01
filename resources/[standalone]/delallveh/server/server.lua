beee35cd251b58583992f5e499a82d9d719e70d7
luap710 <92261388+itsluap@users.noreply.github.com>
2023-11-01 07:18:25 -0500

    move delallveh

--- resources/[standalone]/delallveh/server/server.lua
+++ resources/[standalone]/delallveh/server/server.lua
@@ -4,14 +4,3 @@
 --------------------------------------
 
 RegisterCommand(Config.commandName, function(source, args, rawCommand) TriggerClientEvent("wld:delallveh", -1) end, Config.restricCommand)
-
--- testing ox_lib --
-
-local i = 0
- 
-while true do
-    Wait(1000)
-    i += 1
- 
-    print(cache('test', function() return i end, 5000))
-end
