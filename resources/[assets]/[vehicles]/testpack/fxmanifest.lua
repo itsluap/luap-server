83f8b6f988bc9e13ff94e09031aac203d85f981a
itsluap <92261388+itsluap@users.noreply.github.com>
2023-11-01 07:15:25 -0500

    cars

---
+++ resources/[assets]/[vehicles]/testpack/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+description "Car Add-on"
+
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+client_script 'names.lua'
+
+files {
+    'carcols.meta',
+    'data/**/handling.meta',
+    'data/**/vehiclelayouts.meta',
+    'data/**/vehicles.meta',
+    'data/**/carcols.meta',
+    'data/**/carvariations.meta',
+    'data/**/vehicle_names.meta',
+}
