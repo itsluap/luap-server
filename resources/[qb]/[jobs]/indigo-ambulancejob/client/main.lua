fcdacfb1090103320684270a38448aebbff092ac
luap710 <92261388+itsluap@users.noreply.github.com>
2023-11-02 02:32:08 -0500

    .

--- resources/[qb]/[jobs]/indigo-ambulancejob/client/main.lua
+++ resources/[qb]/[jobs]/indigo-ambulancejob/client/main.lua
@@ -338,11 +338,13 @@ local function CheckWeaponDamage(ped)
         if HasPedBeenDamagedByWeapon(ped, k, 0) then
             detected = true
             if not IsInDamageList(k) then
+                --[[
                 TriggerEvent('chat:addMessage', {
                     color = { 255, 0, 0},
                     multiline = false,
                     args = {Lang:t('info.status'), v.damagereason}
                 })
+                ]]--
                 CurrentDamageList[#CurrentDamageList+1] = k
             end
         end
