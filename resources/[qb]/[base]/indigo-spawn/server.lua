a37a94aba9f03c7dfb8aedbb5c181f08396fc036
luap710 <92261388+itsluap@users.noreply.github.com>
2023-11-02 22:27:14 -0500

    .

--- resources/[qb]/[base]/indigo-spawn/server.lua
+++ resources/[qb]/[base]/indigo-spawn/server.lua
@@ -1,25 +1,10 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
---[[
 QBCore.Functions.CreateCallback('indigo-spawn:server:getOwnedHouses', function(_, cb, cid)
     if cid ~= nil then
         local houses = MySQL.query.await('SELECT * FROM player_houses WHERE citizenid = ?', {cid})
         if houses[1] ~= nil then
             print(json.encode(houses))
-            cb(houses)
-        else
-            cb({})
-        end
-    else
-        cb({})
-    end
-end)
-]]--
-
-QBCore.Functions.CreateCallback('indigo-spawn:server:getOwnedHouses', function(_, cb, cid)
-    if cid ~= nil then
-        local houses = MySQL.query.await('SELECT * FROM properties WHERE owner_citizenid = ?', {cid})
-        if houses[1] ~= nil then
             cb(houses)
         else
             cb({})
--- resources/[qb]/[vehicles]/indigo-garages/server/main.lua
+++ resources/[qb]/[vehicles]/indigo-garages/server/main.lua
@@ -176,7 +176,7 @@ QBCore.Functions.CreateCallback("qb-garage:server:checkOwnership", function(sour
          MySQL.query('SELECT * FROM player_vehicles WHERE plate = ?', {plate}, function(result)
             if result[1] then
                 if not UseLoafHousing then
-                    hasHouseKey = exports['ps-housing']:IsOwner(src, house)
+                    hasHouseKey = exports['indigo-houses']:hasKey(result[1].license, result[1].citizenid, garage)
                 end
                 if hasHouseKey then
                     cb(true)
