2351bc1949a125580ea9143d53e697e364ba5392
luap710 <92261388+itsluap@users.noreply.github.com>
2023-11-03 00:26:26 -0500

    Update cfg_robbery.lua

--- resources/[qb]/[crime]/[robbery]/[nuno-radio-man]/core_rob_bank_pacific_gabz/cfg/cfg_robbery.lua
+++ resources/[qb]/[crime]/[robbery]/[nuno-radio-man]/core_rob_bank_pacific_gabz/cfg/cfg_robbery.lua
@@ -275,15 +275,15 @@ cfg.robbery = {
 				model = "", -- model_hash
 				models = {
 					--{"none",50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_money_01a"),50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_money_01b"),50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_money_01c"),50.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_money_01a"),55.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_money_01b"),55.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_money_01c"),55.0}, -- {model_hash,percentage}
 					--{GetHashKey("interactable_trolly_silver_01a"),0.0}, -- {model_hash,percentage}
 					--{GetHashKey("interactable_trolly_silver_01b"),0.0}, -- {model_hash,percentage}
 					--{GetHashKey("interactable_trolly_silver_01c"),0.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_gold_01a"),50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_gold_01b"),50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_gold_01c"),50.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_gold_01a"),45.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_gold_01b"),45.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_gold_01c"),45.0}, -- {model_hash,percentage}
 				},
 				position = {241.64663,210.94276,97.11051,0.0,0.0,340.0,0.0,0.0,-0.51,0.0}, -- {pos_x,pos_y,pos_z,rot_x,rot_y,rot_z,offset_x,offset_y,offset_z,offset_heading}
 				positions = {},
@@ -459,15 +459,15 @@ cfg.robbery = {
 				model = "", -- model_hash
 				models = {
 					--{"none",50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_money_01a"),50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_money_01b"),50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_money_01c"),50.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_money_01a"),55.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_money_01b"),55.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_money_01c"),55.0}, -- {model_hash,percentage}
 					--{GetHashKey("interactable_trolly_silver_01a"),0.0}, -- {model_hash,percentage}
 					--{GetHashKey("interactable_trolly_silver_01b"),0.0}, -- {model_hash,percentage}
 					--{GetHashKey("interactable_trolly_silver_01c"),0.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_gold_01a"),50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_gold_01b"),50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_gold_01c"),50.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_gold_01a"),45.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_gold_01b"),45.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_gold_01c"),45.0}, -- {model_hash,percentage}
 				},
 				position = {252.19699,239.92962,97.11051,0.0,0.0,160.0,0.0,0.0,-0.51,0.0}, -- {pos_x,pos_y,pos_z,rot_x,rot_y,rot_z,offset_x,offset_y,offset_z,offset_heading}
 				positions = {},
@@ -644,15 +644,15 @@ cfg.robbery = {
 				model = "", -- model_hash
 				models = {
 					--{"none",50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_money_01a"),50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_money_01b"),50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_money_01c"),50.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_money_01a"),55.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_money_01b"),55.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_money_01c"),55.0}, -- {model_hash,percentage}
 					--{GetHashKey("interactable_trolly_silver_01a"),0.0}, -- {model_hash,percentage}
 					--{GetHashKey("interactable_trolly_silver_01b"),0.0}, -- {model_hash,percentage}
 					--{GetHashKey("interactable_trolly_silver_01c"),0.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_gold_01a"),50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_gold_01b"),50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_gold_01c"),50.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_gold_01a"),45.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_gold_01b"),45.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_gold_01c"),45.0}, -- {model_hash,percentage}
 				},
 				position = {229.38514,234.68666,97.11051,0.0,0.0,160.0,0.0,0.0,-0.51,0.0}, -- {pos_x,pos_y,pos_z,rot_x,rot_y,rot_z,offset_x,offset_y,offset_z,offset_heading}
 				positions = {},
@@ -758,15 +758,15 @@ cfg.robbery = {
 				model = "", -- model_hash
 				models = {
 					--{"none",50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_money_01a"),50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_money_01b"),50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_money_01c"),50.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_money_01a"),55.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_money_01b"),55.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_money_01c"),55.0}, -- {model_hash,percentage}
 					--{GetHashKey("interactable_trolly_silver_01a"),0.0}, -- {model_hash,percentage}
 					--{GetHashKey("interactable_trolly_silver_01b"),0.0}, -- {model_hash,percentage}
 					--{GetHashKey("interactable_trolly_silver_01c"),0.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_gold_01a"),50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_gold_01b"),50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_gold_01c"),50.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_gold_01a"),45.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_gold_01b"),45.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_gold_01c"),45.0}, -- {model_hash,percentage}
 				},
 				position = {230.96690,234.11094,97.11051,0.0,0.0,160.0,0.0,0.0,-0.51,0.0}, -- {pos_x,pos_y,pos_z,rot_x,rot_y,rot_z,offset_x,offset_y,offset_z,offset_heading}
 				positions = {},
@@ -1055,15 +1055,15 @@ cfg.robbery = {
 				model = "", -- model_hash
 				models = {
 					--{"none",50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_money_01a"),50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_money_01b"),50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_money_01c"),50.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_money_01a"),55.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_money_01b"),55.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_money_01c"),55.0}, -- {model_hash,percentage}
 					--{GetHashKey("interactable_trolly_silver_01a"),0.0}, -- {model_hash,percentage}
 					--{GetHashKey("interactable_trolly_silver_01b"),0.0}, -- {model_hash,percentage}
 					--{GetHashKey("interactable_trolly_silver_01c"),0.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_gold_01a"),50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_gold_01b"),50.0}, -- {model_hash,percentage}
-					{GetHashKey("interactable_trolly_gold_01c"),50.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_gold_01a"),45.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_gold_01b"),45.0}, -- {model_hash,percentage}
+					{GetHashKey("interactable_trolly_gold_01c"),45.0}, -- {model_hash,percentage}
 				},
 				position = {225.58288,226.80377,97.11051,0.0,0.0,340.0,0.0,0.0,-0.51,0.0}, -- {pos_x,pos_y,pos_z,rot_x,rot_y,rot_z,offset_x,offset_y,offset_z,offset_heading}
 				positions = {},
