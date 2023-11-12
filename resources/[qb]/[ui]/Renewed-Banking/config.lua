8a4757e9a184d5c0a2ad4306f1165fb4bee7391c
luap710 <92261388+itsluap@users.noreply.github.com>
2023-11-12 00:10:04 -0600

    Update config.lua

--- resources/[qb]/[ui]/Renewed-Banking/config.lua
+++ resources/[qb]/[ui]/Renewed-Banking/config.lua
@@ -13,32 +13,32 @@ Config = {
     peds = {
         [1] = { -- Pacific Standard
             model = 'u_m_m_bankman',
-            coords = vector4(266.7939, 224.6467, 106.2822, 157.4950)
+            coords = vector4(266.7939, 224.6467, 106.2822, 157.4950),
             createAccounts = true
         },
         [2] = {
             model = 'ig_barry',
-            coords = vector4(313.84, -280.58, 54.16, 338.31)
+            coords = vector4(313.84, -280.58, 54.16, 338.31),
         },
         [3] = {
             model = 'ig_barry',
-            coords = vector4(149.46, -1042.09, 29.37, 335.43)
+            coords = vector4(149.46, -1042.09, 29.37, 335.43),
         },
         [4] = {
             model = 'ig_barry',
-            coords = vector4(-351.23, -51.28, 49.04, 341.73)
+            coords = vector4(-351.23, -51.28, 49.04, 341.73),
         },
         [5] = {
             model = 'ig_barry',
-            coords = vector4(-1211.9, -331.9, 37.78, 20.07)
+            coords = vector4(-1211.9, -331.9, 37.78, 20.07),
         },
         [6] = {
             model = 'ig_barry',
-            coords = vector4(-2961.14, 483.09, 15.7, 83.84)
+            coords = vector4(-2961.14, 483.09, 15.7, 83.84),
         },
         [7] = {
             model = 'ig_barry',
-            coords = vector4(1174.8, 2708.2, 38.09, 178.52)
+            coords = vector4(1174.8, 2708.2, 38.09, 178.52),
         },
         [8] = { -- paleto
             model = 'u_m_m_bankman',
