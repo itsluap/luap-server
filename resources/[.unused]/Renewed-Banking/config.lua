95f8cbce5fdff29eaddc99e8963b33ab45bb8ff3
luap710 <92261388+itsluap@users.noreply.github.com>
2023-11-12 00:09:06 -0600

    .

---
+++ resources/[.unused]/Renewed-Banking/config.lua
@@ -0,0 +1,51 @@
+config = {
+    renewedMultiJob = false, -- https://github.com/Renewed-Scripts/indigo-phone Built into the indigo-phone edit.
+    atms = {
+        `prop_atm_01`,
+        `prop_atm_02`,
+        `prop_atm_03`,
+        `prop_fleeca_atm`,
+    },
+    peds = {
+        [1] = { -- Pacific Standard
+            model = 'u_m_m_bankman',
+            coords = vector4(266.7939, 224.6467, 106.2822, 157.4950),
+            createAccounts = true,
+        },
+        [2] = {
+            model = 'ig_barry',
+            coords = vector4(313.84, -280.58, 54.16, 338.31),
+            createAccounts = true,
+        },
+        [3] = {
+            model = 'ig_barry',
+            coords = vector4(149.46, -1042.09, 29.37, 335.43),
+            createAccounts = true,
+        },
+        [4] = {
+            model = 'ig_barry',
+            coords = vector4(-351.23, -51.28, 49.04, 341.73),
+            createAccounts = true,
+        },
+        [5] = {
+            model = 'ig_barry',
+            coords = vector4(-1211.9, -331.9, 37.78, 20.07),
+            createAccounts = true,
+        },
+        [6] = {
+            model = 'ig_barry',
+            coords = vector4(-2961.14, 483.09, 15.7, 83.84),
+            createAccounts = true,
+        },
+        [7] = {
+            model = 'ig_barry',
+            coords = vector4(1174.8, 2708.2, 38.09, 178.52),
+            createAccounts = true,
+        },
+        [8] = { -- paleto
+            model = 'u_m_m_bankman',
+            coords = vector4(-112.22, 6471.01, 31.63, 134.18),
+            createAccounts = true,
+        }
+    }
+}
