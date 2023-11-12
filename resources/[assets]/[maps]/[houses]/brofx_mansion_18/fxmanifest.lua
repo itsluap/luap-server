2fc37efc47bacb1f289f021788a0b6f1ec98e9c8
luap710 <92261388+itsluap@users.noreply.github.com>
2023-11-12 13:50:09 -0600

    map updates

--- resources/[assets]/[maps]/[houses]/brofx_mansion_18/fxmanifest.lua
+++ resources/[assets]/[maps]/[houses]/brofx_mansion_18/fxmanifest.lua
@@ -3,24 +3,9 @@ game "gta5"
 
 author 'brofx'
 description 'brofx_mansion_18'
-version '1.0.0'
+version '1.4.0'
 
 this_is_a_map "yes"
-
-files {
-    'audio/brofx_18_doorsounds_game.dat151.rel',
-    'audio/brofx_mansion18_collision_game.dat151.rel',
-    'audio/brofx_mansion18_collision_mix.dat15.rel',
-    'audio/brofx_mansion18_garage_collision_game.dat151.rel',
-    'audio/brofx_mansion18_garage_collision_mix.dat15.rel',
-}
-
-data_file 'AUDIO_GAMEDATA' 'audio/brofx_18_doorsounds_game.dat'
-data_file 'AUDIO_GAMEDATA' 'audio/brofx_mansion18_collision_game.dat'
-data_file 'AUDIO_DYNAMIXDATA' 'audio/brofx_mansion18_collision_mix.dat'
-data_file 'AUDIO_GAMEDATA' 'audio/brofx_mansion18_garage_collision_game.dat'
-data_file 'AUDIO_DYNAMIXDATA' 'audio/brofx_mansion18_garage_collision_mix.dat'
-
 
 
 dependency '/assetpacks'
