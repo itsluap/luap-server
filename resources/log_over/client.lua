44103534244687c3bd2675c9f13873ad1926d09a
luap710 <92261388+itsluap@users.noreply.github.com>
2023-11-15 18:46:04 -0600

    .

---
+++ resources/log_over/client.lua
@@ -0,0 +1,29 @@
+-- Oversized Texture Logger Script
+
+-- Specify the path to the log file
+local logFilePath = "path/to/oversized_texture_log.txt"
+
+-- Function to log oversized texture warnings
+function logOversizedTexture(message)
+    local timestamp = os.date("[%Y-%m-%d %H:%M:%S]")
+    local logMessage = timestamp .. " " .. message
+
+    -- Open the log file in append mode
+    local logFile = io.open(logFilePath, "a")
+
+    if logFile then
+        -- Write the log message and close the file
+        logFile:write(logMessage .. "\n")
+        logFile:close()
+    else
+        print("Error: Unable to open log file for writing")
+    end
+end
+
+-- Event handler for the print event
+AddEventHandler("print", function(message)
+    -- Check if the message contains the keyword indicating an oversized texture warning
+    if string.find(message, "oversized_texture_warning_keyword") then
+        logOversizedTexture(message)
+    end
+end)
