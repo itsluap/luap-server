e6450d8322c2bd5d2e80d9ee53deb231e6eddb5b
itsluap <92261388+itsluap@users.noreply.github.com>
2023-11-23 12:45:07 -0600

    .

---
+++ resources/[qb]/[ui]/indigo-doorlock/configs/troopermlo.lua
@@ -0,0 +1,136 @@
+
+
+-- 1 created by luap
+Config.DoorList['troopermlo-1'] = {
+    authorizedJobs = { ['trooper'] = 0 },
+    doors = {
+        {objName = -584365942, objYaw = 240.0, objCoords = vec3(1536.117432, 818.643738, 77.806427)},
+        {objName = -584365942, objYaw = 59.999977111816, objCoords = vec3(1537.418457, 820.897339, 77.806427)}
+    },
+    pickable = true,
+    doorRate = 1.0,
+    distance = 2,
+    doorLabel = 'front door',
+    doorType = 'double',
+    locked = true,
+}
+
+-- 2 created by luap
+Config.DoorList['troopermlo-2'] = {
+    authorizedJobs = { ['trooper'] = 0 },
+    doorRate = 1.0,
+    fixText = false,
+    objCoords = vec3(1539.244751, 822.143188, 77.792702),
+    objName = 1040797377,
+    pickable = true,
+    objYaw = 330.0,
+    distance = 2,
+    doorLabel = 'to dispatch',
+    locked = true,
+    doorType = 'door',
+}
+
+-- 3 created by luap
+Config.DoorList['troopermlo-3'] = {
+    authorizedJobs = { ['trooper'] = 0 },
+    doorRate = 1.0,
+    fixText = false,
+    objCoords = vec3(1541.448608, 812.184998, 77.792702),
+    objName = 1040797377,
+    pickable = true,
+    objYaw = 59.999977111816,
+    distance = 2,
+    doorLabel = 'to hallway',
+    locked = true,
+    doorType = 'door',
+}
+
+-- 4 created by luap
+Config.DoorList['troopermlo-4'] = {
+    authorizedJobs = { ['trooper'] = 0 },
+    doorRate = 1.0,
+    fixText = false,
+    objCoords = vec3(1565.421509, 830.743042, 76.656738),
+    objName = -1671593055,
+    pickable = true,
+    objYaw = 330.0,
+    distance = 2,
+    doorLabel = 'cell 1',
+    locked = true,
+    doorType = 'door',
+}
+
+-- 5 created by luap
+Config.DoorList['troopermlo-5'] = {
+    authorizedJobs = { ['trooper'] = 0 },
+    doorRate = 1.0,
+    fixText = false,
+    objCoords = vec3(1563.922363, 828.150269, 76.656738),
+    objName = -1671593055,
+    pickable = true,
+    objYaw = 330.0,
+    distance = 2,
+    doorLabel = 'cell 2',
+    locked = true,
+    doorType = 'door',
+}
+
+-- 6 created by luap
+Config.DoorList['troopermlo-6'] = {
+    authorizedJobs = { ['trooper'] = 0 },
+    doorRate = 1.0,
+    fixText = false,
+    objCoords = vec3(1561.424316, 833.050964, 76.656738),
+    objName = -1671593055,
+    pickable = true,
+    objYaw = 330.0,
+    distance = 2,
+    doorLabel = 'cell 3',
+    locked = true,
+    doorType = 'door',
+}
+
+-- 7 created by luap
+Config.DoorList['troopermlo-7'] = {
+    authorizedJobs = { ['trooper'] = 0 },
+    doorRate = 1.0,
+    fixText = false,
+    objCoords = vec3(1559.924927, 830.457886, 76.656738),
+    objName = -1671593055,
+    pickable = true,
+    objYaw = 330.0,
+    distance = 2,
+    doorLabel = 'cell 4',
+    locked = true,
+    doorType = 'door',
+}
+
+-- 8 created by luap
+Config.DoorList['troopermlo-8'] = {
+    authorizedJobs = { ['trooper'] = 0 },
+    doorRate = 1.0,
+    fixText = false,
+    objCoords = vec3(1559.061890, 833.178528, 76.656738),
+    objName = -1671593055,
+    pickable = true,
+    objYaw = 59.999977111816,
+    distance = 2,
+    doorLabel = 'to cells',
+    locked = true,
+    doorType = 'door',
+}
+
+-- 9 created by luap
+Config.DoorList['troopermlo-9'] = {
+    authorizedJobs = { ['trooper'] = 0 },
+    doorRate = 1.0,
+    fixText = false,
+    objCoords = vec3(1551.010376, 819.850281, 77.787933),
+    objName = -344890090,
+    pickable = true,
+    objYaw = 240.00001525879,
+    distance = 2,
+    doorLabel = 'backdoor',
+    locked = true,
+    doorType = 'door',
+}
