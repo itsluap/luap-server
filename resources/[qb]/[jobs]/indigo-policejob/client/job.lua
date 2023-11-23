276ab3c92db8ee7417ccb4614d6ed6728370284f
luap710 <92261388+itsluap@users.noreply.github.com>
2023-11-23 16:29:36 -0600

    Update job.lua

--- resources/[qb]/[jobs]/indigo-policejob/client/job.lua
+++ resources/[qb]/[jobs]/indigo-policejob/client/job.lua
@@ -651,8 +651,10 @@ local function heli()
             Wait(0)
             if inHelicopter and PlayerData.job.type == 'leo' then
                 if PlayerJob.onduty then sleep = 5 end
+                print("can press E to spawn heli")
                 if IsControlJustReleased(0, 38) then
                     TriggerEvent("qb-police:client:spawnHelicopter")
+                    print("spawning heli")
                     break
                 end
             else
