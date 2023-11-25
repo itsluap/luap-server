934d70672d239f16b18bbe364b301347b48e5923
luap710 <92261388+itsluap@users.noreply.github.com>
2023-11-25 00:29:37 -0600

    Update main.lua

--- resources/[qb]/[vehicles]/indigo-garages/client/main.lua
+++ resources/[qb]/[vehicles]/indigo-garages/client/main.lua
@@ -816,7 +816,7 @@ RegisterNetEvent('indigo-garages:client:OpenMenu', function()
             else
                 print("You are not the owner.")
             end
-        end, propertyId)
+        end, property_id)
     end
 end)
 
